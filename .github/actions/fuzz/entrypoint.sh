91f53bcd9ada0d22bb2ebe9ab94ec4f43bf7a31d
Christiaan Herrewijn <christiaan@duckdblabs.com>
2025-01-22 16:37:23 +0100

    generate dummy artifact

--- .github/actions/fuzz/entrypoint.sh
+++ .github/actions/fuzz/entrypoint.sh
@@ -16,4 +16,9 @@ echo "entrypoint received fuzzer name: $FUZZER_NAME"
 # output: set some var
 echo "fuzz_result='dummy fuzz output for fuzzer: $FUZZER_NAME'" >> "$GITHUB_OUTPUT"
 
+# output: store in file
+FUZZ_RESULT_FILE=dummy-fuzz-result.txt
+echo "fuzz_result='dummy fuzz output for fuzzer: $FUZZER_NAME'" > $FUZZ_RESULT_FILE
+echo "fuzz_result_file=$FUZZ_RESULT_FILE" >> "$GITHUB_OUTPUT"
+
 exit 0
